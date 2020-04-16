3e241d6860d5a92882e6e3519a9c167f7c8bce2c
F1sher-Man <fischbach.karol96@gmail.com>
2020-04-16 19:10:50 +0200

    Header item created. Test commit also.

--- src/components/common/Header/Header.tsx
+++ src/components/common/Header/Header.tsx
@@ -7,6 +7,10 @@ const Header: React.FC<HeaderProps> = () => {
   return (
     <div>
       <h1 className="header-sign">justjoin.this</h1>
+      <div className="item-container">
+        <div className="nav-items"></div>
+        <div className="btn-items"></div>
+      </div>
       <hr />
     </div>
   );
---
+++ src/components/common/Header/HeaderItem.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export interface HeaderItemProps {}
+
+const HeaderItem: React.FC<HeaderItemProps> = () => {
+  return <div>d00ppa</div>;
+};
+
+export default HeaderItem;
