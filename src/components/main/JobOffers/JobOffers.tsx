b482132ecd27a6ab1b99a0263590a9e875d186b3
F1sher-Man <fischbach.karol96@gmail.com>
2020-04-25 16:18:42 +0200

    Creating offer component. Almost done - mobile view issues.

--- src/components/main/JobOffers/JobOffers.tsx
+++ src/components/main/JobOffers/JobOffers.tsx
@@ -1,61 +1,26 @@
 import * as React from "react";
 import DropdownLocation from "./DropdownLocation";
 import Technologies from "./Technologies";
-import '../../../styles/main/JobOffers/JobOffers.scss';
+import Offers from "./Offers";
+import "../../../styles/main/JobOffers/JobOffers.scss";
 
-export interface JobOffersProps { }
+export interface JobOffersProps {}
 
 const JobOffers: React.FC<JobOffersProps> = () => {
-
-  return <div className="page-container">
-    <div className="topbar">
-      <DropdownLocation />
-      <Technologies />
-    </div>
-    <div className="content-container">
-      <div className="offers">
-        ppp<br />
-        pp<br />
-        ppp<br />
-        pp<br />ppp<br />
-        pp<br />
-        ppp<br />
-        pp<br />
-        ppp<br />
-        pp<br />ppp<br />
-        pp<br />ppp<br />
-        pp<br />
-        ppp<br />
-        pp<br />ppp<br />
-        pp<br />ppp<br />
-        pp<br />
-        ppp<br />
-        pp<br />ppp<br />
-        pp<br />ppp<br />
-        pp<br />
-        ppp<br />
-        pp<br />ppp<br />
-        pp<br />ppp<br />
-        pp<br />
-        ppp<br />
-        pp<br />ppp<br />
-        pp<br />ppp<br />
-        pp<br />
-        ppp<br />
-        pp<br />ppp<br />
-        pp<br />ppp<br />
-        pp<br />
-        ppp<br />
-        pp<br />ppp<br />
-        pp<br />
+  return (
+    <div className="page-container">
+      <div className="topbar">
+        <DropdownLocation />
+        <Technologies />
       </div>
-      <div className="map">
-        ss
+      <div className="content-container">
+        <div className="offers">
+          <Offers />
+        </div>
+        <div className="map">ss</div>
       </div>
     </div>
-
-
-  </div >;
+  );
 };
 
 export default JobOffers;
---
+++ src/components/main/JobOffers/Offers.tsx
@@ -0,0 +1,66 @@
+import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCreativeCommonsSampling } from "@fortawesome/free-brands-svg-icons";
+import { faBuilding, faMapMarkedAlt } from "@fortawesome/free-solid-svg-icons";
+import "../../../styles/main/JobOffers/Offers.scss";
+export interface OffersProps {}
+
+const Offers: React.FC<OffersProps> = () => {
+  return (
+    <div className="offer-container">
+      <div className="offer">
+        <div className="strip-icon">
+          <div className="strip" />
+          <div className="company-icon">
+            <FontAwesomeIcon icon={faCreativeCommonsSampling} size="4x" />
+          </div>
+        </div>
+        <div className="desc-company">
+          <div className="desc">
+            <b>JavaScript Senior Dev</b>
+          </div>
+          <div className="company">
+            <FontAwesomeIcon icon={faBuilding} />
+            Tick Company
+            <FontAwesomeIcon icon={faMapMarkedAlt} />
+            Warsaw
+          </div>
+        </div>
+        <div className="salary-stack">
+          <div className="salary">14000-21000 PLN</div>
+          <div className="stack">
+            <div className="stack-item">JS</div>
+            <div className="stack-item">Node</div>
+            <div className="stack-item">Angular</div>
+            <div className="stack-item">Jira</div>
+          </div>
+        </div>
+      </div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+      <div className="offer">dupal</div>
+    </div>
+  );
+};
+
+export default Offers;
