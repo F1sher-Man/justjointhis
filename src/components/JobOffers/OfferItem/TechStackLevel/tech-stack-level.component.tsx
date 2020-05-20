897979eae4ce0293e8abf978dbb7c62f670f7430
F1sher-Man <fischbach.karol96@gmail.com>
2020-05-20 22:25:32 +0200

    Finishing view of detailed offer - tech stack dots, desc finished; apply form to be finished

---
+++ src/components/JobOffers/OfferItem/TechStackLevel/tech-stack-level.component.tsx
@@ -0,0 +1,34 @@
+import React, { ReactElement } from "react";
+import "./tech-stack-level.component.scss";
+
+export interface TechStackLevelProps {
+  desc: String;
+  level: number;
+}
+
+const drawCircles = (level: number, filled?: boolean): any => {
+  let circles: Array<any> = [];
+  for (let k = 0; k < level; k++) {
+    circles.push(
+      <div
+        className="tech-dot"
+        style={{ backgroundColor: filled ? "#ff4081" : "#bfc5d2" }}
+      />
+    );
+  }
+  return circles;
+};
+
+const TechStackLevel = ({ desc, level }: TechStackLevelProps): ReactElement => {
+  return (
+    <div className="tech-stack-container">
+      <div className="dots-container">
+        {drawCircles(level, true)}
+        {drawCircles(5 - level)}
+      </div>
+      <p>{desc}</p>
+    </div>
+  );
+};
+
+export default TechStackLevel;
--- src/components/JobOffers/OfferItem/offer-item-detailed.component.tsx
+++ src/components/JobOffers/OfferItem/offer-item-detailed.component.tsx
@@ -2,6 +2,10 @@ import React, { ReactElement } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCreativeCommonsSampling } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelopeOpenText } from "@fortawesome/free-solid-svg-icons";
+import TechStackElement from "./TechStackLevel/tech-stack-level.component";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import CloudUploadIcon from "@material-ui/icons/CloudUpload";
 
 import "./offer-item-detailed.component.scss";
 
@@ -18,12 +22,13 @@ const OfferDetailed = ({ goBack }: OfferDetailedProps): ReactElement => {
 
   type stackType = {
     desc: String;
-    level: Number;
+    level: number;
   };
   const techStack: Array<stackType> = [
     { desc: "JS", level: 4 },
-    { desc: "HTML", level: 4 },
+    { desc: "HTML", level: 3 },
     { desc: "CSS", level: 2 },
+    { desc: "AGILE", level: 1 },
   ];
 
   return (
@@ -50,21 +55,93 @@ const OfferDetailed = ({ goBack }: OfferDetailedProps): ReactElement => {
           <p>Tech Stack</p>
           <hr />
           <div className="techs">
-            <div>
-              <p>React</p>
-              {techStack.map((element) => {
-                return (
-                  <div>
-                    <p>{element.desc}</p>
-                    {for (let i = 0; i < 9; i++) {
-   console.log(i);}
-                    }}
-                    <div className="tech-dot" />
-                  </div>
-                );
-              })}
-            </div>
+            {techStack.map((tech) => (
+              <TechStackElement desc={tech.desc} level={tech.level} />
+            ))}
           </div>
+        </div>
+        <div className="offer-container-item-stack">
+          <p>Description</p>
+          <hr />
+          <p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
+              nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi.
+              Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum.
+              Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris
+              massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti
+              sociosqu ad litora torquent per conubia nostra, per inceptos
+              himenaeos. Curabitur sodales ligula in libero. Sed dignissim
+              lacinia nunc.{" "}
+            </p>
+
+            <p>
+              Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque
+              sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin
+              ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis
+              vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula
+              lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel,
+              tincidunt sed, euismod in, nibh. Quisque volutpat condimentum
+              velit. Class aptent taciti sociosqu ad litora torquent per conubia
+              nostra, per inceptos himenaeos.{" "}
+            </p>
+
+            <p>
+              Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque
+              adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi.{" "}
+              <i>Pellentesque nibh</i>. Ut fringilla. Suspendisse potenti. Nunc
+              feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin
+              quam. Etiam ultrices. Suspendisse in justo eu magna luctus
+              suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque
+              cursus, metus vitae pharetra auctor, sem massa mattis sem, at
+              interdum magna augue eget diam.{" "}
+            </p>
+          </p>
+        </div>
+        <div className="offer-container-item-stack">
+          <p>Apply for a job</p>
+          <hr />
+          <form noValidate autoComplete="off" className="apply-form">
+            <div className="apply-form-textfields">
+              <TextField
+                style={{ backgroundColor: "white" }}
+                id="outlined-basic"
+                label="Imię i nazwisko"
+                variant="outlined"
+              />
+              <TextField
+                style={{ backgroundColor: "white" }}
+                id="outlined-basic"
+                label="E-mail"
+                variant="outlined"
+              />
+            </div>
+            <div>
+              <input
+                accept="image/*"
+                id="raised-button-file"
+                multiple
+                type="file"
+                style={{ display: "none" }}
+              />
+              <label htmlFor="raised-button-file">
+                <Button
+                  variant="contained"
+                  color="default"
+                  component="span"
+                  startIcon={<CloudUploadIcon />}
+                >
+                  <b>CV</b>
+                </Button>
+              </label>
+              <p>CV : brak pliku</p>
+            </div>
+            <div>
+              <Button variant="contained" color="primary">
+                <b>Aplikuj</b>
+              </Button>
+            </div>
+          </form>
         </div>
       </div>
     </div>
